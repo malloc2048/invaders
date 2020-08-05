8ce9c5d9b3d70b7b90471f5a09c48ee35244557a
malloc <jjmaroulis@gmail.com>
2020-08-05 14:24:30 -0400

    better test coverage

--- test/cabinet/instructions/OpCodeTestFixture.cpp
+++
@@ -1,8 +0,0 @@
-#include "OpCodeTestFixture.h"
-
-void OpCodeTestFixture::SetUp() {
-    Test::SetUp();
-    registers.pc.d16 = cabinet::Memory::ROM_SIZE;
-    memory.write(cabinet::Memory::ROM_SIZE, 0x01);
-    memory.write(cabinet::Memory::ROM_SIZE + 1, 0x02);
-}
